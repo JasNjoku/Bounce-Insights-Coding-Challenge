eb15f7e1bafdb0cffbefc1f3780e4d8d5156a087
Jason Njoku <JasNjoku@gmail.com>
2023-11-29 21:44:43 +0000

    Create variable to show the users search query

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchBar from './components/SearchBar';
 import './styles/App.css';
 import axios from 'axios'
+import CountryBox from './components/CountryBox';
 
 function App() {
 
-  const [country, setCountry] = useState()
+  const [countries, setCountries] = useState([])
+  const [query, setQuery] = useState("")
+
+  useEffect(() => {
+    
+  }, [countries])
 
   const search = async (query) => {
     const response = await axios.get(`http://localhost:4000/countries/name/${query}`)
-    setCountry(response.data)
-    console.log(response.data)
+    setQuery(query)
+    setCountries(response.data)
   }
 
+  
   return (
     <div className="App">
       <SearchBar searchFunc={search}/>
+      {query === "" ? null : <>Showing results for {query} ({countries.length})</>}
+      {countries.length < 1 ? null : countries.map(country => {return <CountryBox key={country.name} country={country}/>})}
     </div>
   );
 }
