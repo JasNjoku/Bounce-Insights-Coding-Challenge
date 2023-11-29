c6248600ac0c8060d7485cf722a6251a6780993d
Jason Njoku <JasNjoku@gmail.com>
2023-11-29 13:49:15 +0000

    Create searchbar component to accept user query

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import axios from "axios"
+
+const SearchBar = (props) => {
+
+    
+    const search = (e) => {
+        e.preventDefault();
+        const searchRequest = e.target.querySelector('input').value;
+
+        props.searchFunc(searchRequest)
+    }
+
+    return (
+        <form onSubmit={search}>
+            <input placeholder="Country name"/>
+            <button type="submit">Search</button>
+        </form>
+    )
+}
+
+export default SearchBar;
