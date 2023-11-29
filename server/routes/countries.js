6fc8150d9741adedb62e5f02b4428ca2236f997e
Jason Njoku <JasNjoku@gmail.com>
2023-11-29 21:40:51 +0000

    Format response.data in server

--- server/routes/countries.js
+++ server/routes/countries.js
@@ -2,12 +2,35 @@ const router = require('express').Router()
 const axios = require('axios').default
 
 const fetchCountry = async (searchRequest) => {
+
     try {
+        countryData = []
+
         const response = await axios.get(`https://restcountries.com/v3.1/name/${searchRequest}`)
-        return response.data
+
+        //Check if the response data contains countries
+        if (response.status === 200) {
+            response.data.forEach(country => {
+                countryData.push({
+                    capital: country.capital,
+                    continent: country.continents,
+                    currencies: country.currencies,
+                    flagImg: country.flags.png,
+                    googleMapsUrl: country.maps.googleMaps,
+                    name: country.name.common,
+                    population: country.population,
+                    subRegion: country.subregion,
+                    timezone: country.timezones
+                })
+            })
+        }
+
+        return countryData
     } catch (error) {
         console.error(error)
     }
+
+
 }
 
 router.get('/countries/name/:searchRequest', async (req, res) => {
