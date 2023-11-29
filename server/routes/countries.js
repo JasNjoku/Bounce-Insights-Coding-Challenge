38e86ec4abac5a41ad929a7bc562107442ff49cb
Jason Njoku <JasNjoku@gmail.com>
2023-11-29 12:08:59 +0000

    Create basic function to fetch country data

---
+++ server/routes/countries.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const axios = require('axios').default
+
+
+const fetchCountry = async (searchRequest) => {
+    try {
+        const response = await axios.get(`https://restcountries.com/v3.1/name/${searchRequest}`)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+router.get('/countries/name/:searchRequest', async (req, res) => {
+    const { searchRequest } = req.params
+    res.json(await fetchCountry(searchRequest))
+})
+
+module.exports = router
