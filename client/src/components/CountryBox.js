8984739d2930941d699a80408de1dca708600d21
Jason Njoku <JasNjoku@gmail.com>
2023-12-01 17:41:49 +0000

    Add image alt

--- client/src/components/CountryBox.js
+++ client/src/components/CountryBox.js
@@ -7,7 +7,7 @@ const CountryBox = (props) => {
     return (
         <div className="country-box">
             <div className="country-box-img">
-                <img src={country.flagImg} />
+                <img src={country.flagImg} alt="Country Flag"/>
             </div>
             <p>{country.name}</p>
             <Link to={`/country/${country.name}`} state={{country: country}}>View More</Link>
